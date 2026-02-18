a319c539b2c06f9dcb0972cb6daa7875756be568
sej <ejseog@gmail.com>
2026-02-19 02:45:19 +0900

    - 빌드오류 수정

--- src/components/bm/BmComponent.tsx
+++ src/components/bm/BmComponent.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import BmMainComponent from "./view/BmMainComponent";
 
-const BmComponent = () => {
+type BmComponentProps = {
+  variant?: "main" | "sub";
+};
+
+const BmComponent = ({ variant }: BmComponentProps) => {
   return <BmMainComponent />;
 };
 
